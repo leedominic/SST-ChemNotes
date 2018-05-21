b21ef1258ec0b650bc3bb0e30b807ef7256254d4
wonghongngai <wonghongngai123@gmail.com>
2018-05-21 16:59:10 +0800

    Initial Commit

---
+++ SST ChemNotes/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  SST ChemNotes
+//
+//  Created by Wong Hong Ngai on 21/5/18.
+//  Copyright © 2018 Wong Hong Ngai. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
