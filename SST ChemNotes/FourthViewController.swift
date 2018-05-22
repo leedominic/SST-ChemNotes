d9e31e5bd84ac63daad2ddc66687fb85040b9ab5
wonghongngai <wonghongngai123@gmail.com>
2018-05-22 10:53:32 +0800

    Added screens and accompanying view controller files

---
+++ SST ChemNotes/FourthViewController.swift
@@ -0,0 +1,35 @@
+//
+//  FourthViewController.swift
+//  SST ChemNotes
+//
+//  Created by Wong Hong Ngai on 22/5/18.
+//  Copyright © 2018 Wong Hong Ngai. All rights reserved.
+//
+
+import UIKit
+
+class FourthViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
